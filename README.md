db4b7885d1304f2d58b8855df2c7227d89c78db3
kateryna-nataliia-hanych <kateryna-nataliia_hanych@epam.com>
2025-03-12 16:29:52 +0200

    CSV Updater class is pushed

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # Python-for-DQEs-course
 The repo for Python for Data Quality Engineers course on learn.epam.com
 
-lab1
+### lab1
 - create list of 100 random numbers from 0 to 1000
 - sort list from min to max (without using sort())
 - calculate average for even and odd numbers
 - print both average result in console
 
-lab2
+
+### lab2
 1. create a list of random number of dicts (from 2 to 10)
 dict's random numbers of keys should be letter,
 dict's values should be a number (0-100),
@@ -17,7 +18,8 @@ if dicts have same key, we will take max value, and rename key with dict number 
 if key is only in one dict - take it as is,
 example: {'a_1': 5, 'b': 7, 'c': 35, 'g_2': 42}
 
-lab3
+### lab3
+
 "homEwork:
   tHis iz your homeWork, copy these Text to variable.
 
@@ -34,20 +36,32 @@ lab3
   last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not only Spaces, but ALL whitespaces. I got 87."
 
 
-lab5_classes
+### lab5_classes
+
 Create a tool, which will do user generated news feed:
-1.User select what data type he wants to add
-2.Provide record type required data
-3.Record is published on text file in special format
+
+1. User select what data type he wants to add
+2. Provide record type required data
+3. Record is published on text file in special format
 
 You need to implement:
-1.News – text and city as input. Date is calculated during publishing.
-2.Privat ad – text and expiration date as input. Day left is calculated during publishing.
+1. News – text and city as input. Date is calculated during publishing. 
+2. Private ad – text and expiration date as input. Day left is calculated during publishing.
 
 Expand with additional class, which allow to provide records by text file:
-1.Define your input format (one or many records)
-2.Default folder or user provided file path
-3.Remove file if it was successfully processed
-4.Apply case normalization functionality from lab3
+1. Define your input format (one or many records)
+2. Default folder or user provided file path
+3. Remove file if it was successfully processed
+4. Apply case normalization functionality from lab3
+
+
+Calculate number of words and letters from previous output test file(news_feed.txt).
+
+Create two csv:
+1. word-count (all words are preprocessed in lowercase)
+2. letter, cout_all, count_uppercase, percentage (add header, spacecharacters are not included)
+
+CSVs should be recreated each time new record added.
+
 
 
--- lab5_classes.py
+++ lab5_classes.py
@@ -1,3 +1,4 @@
+import csv
 import re
 from datetime import datetime
 from lab3 import normalize_case
@@ -85,6 +86,71 @@ class ReadFromFile:
             print(f"Some file processing error happened: {e}")
 
 
+class CSVUpdater:
+    @staticmethod
+    def process_news_feed(file_path):
+        try:
+            with open(file_path, 'r') as file:
+                text = file.read()
+
+            all_words = re.findall(r'\b(?!\d+\b)\w+\b', text.lower())
+            words_count = {}
+            for word in all_words:
+
+                if word in words_count:
+                    words_count[word] += 1
+
+                else:
+                    words_count[word] = 1
+
+            # all letters low and upper case with duplication
+            all_letters_duplicated = re.sub(r'[^a-zA-Z]', '', text)
+            from collections import Counter
+            counter_without_case = Counter(all_letters_duplicated.lower())
+            counter_upper_case_letters = Counter(re.sub(r'[^A-Z]', '', all_letters_duplicated))
+            set_of_letters = set(all_letters_duplicated.lower())
+
+            return words_count, counter_without_case, counter_upper_case_letters, set_of_letters
+
+
+
+
+        except FileNotFoundError:
+            print(f"File {file_path} not found.")
+
+    @staticmethod
+    def write_to_csv_word_count(word_count, output_file="word_count.csv"):
+        # Writing the word count CSV file
+        list = [{'word': k, 'count': v} for k, v in word_count.items()]
+
+        with open(output_file, 'w', newline='') as f:
+            count_writer = csv.DictWriter(f, fieldnames=['word', 'count'], delimiter='-')
+            count_writer.writeheader()
+            count_writer.writerows(list)
+
+    @staticmethod
+    def write_to_csv_letter_count(counter_without_case, counter_upper_case_letters, set_of_letters, output_file="letter_count.csv"):
+        # Writing the letter count CSV file
+        with open(output_file, 'w', newline='') as f:
+            writer_csv = csv.writer(f, delimiter=',', quotechar='\'', quoting=csv.QUOTE_NONE)
+            writer_csv.writerow(['letter', 'cout_all', 'count_uppercase', 'percentage'])
+            for letter in set_of_letters:
+                writer_csv.writerow([letter, counter_without_case[letter], counter_upper_case_letters[letter.upper()],
+                                     counter_upper_case_letters[letter.upper()]/counter_without_case[letter]*100])
+
+    @staticmethod
+    def update_csvs(file_path):
+        words_count, counter_without_case, counter_upper_case_letters, set_of_letters = CSVUpdater.process_news_feed(file_path)
+
+        if words_count is not None and counter_without_case is not None:
+            # Recreate and write CSV files
+            CSVUpdater.write_to_csv_word_count(words_count)
+            CSVUpdater.write_to_csv_letter_count(counter_without_case, counter_upper_case_letters, set_of_letters)
+            print("CSV files updated successfully!")
+        else:
+            print("Error processing the file.")
+
+
 class NewsFeed:
 
     @staticmethod
@@ -93,6 +159,9 @@ class NewsFeed:
         with open("news_feed.txt", "a") as file:
             file.write(content + "\n")
             file.write(' ' * 30 + "\n")
+
+        # Call the update_csvs method after saving content to the file
+        CSVUpdater.update_csvs("news_feed.txt")
 
 
 class News(NewsFeed):
@@ -224,3 +293,5 @@ else:
 
     elif type_of_input == '2':
         enter_from_console()
+
+
